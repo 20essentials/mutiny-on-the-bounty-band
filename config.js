9365dd582410103694897465661d797b1d87bdf2
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:39:54 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MUTINY ON THE BOUNTY';
+export let nameSongs = [
+	"TELEKINESIS", /*1*/
+	"BALLET MÉCANIQUE", /*2*/
+	"ICE ICE ICELAND", /*3*/
+	"MKL JKSN", /*4*/
+	"FIN DE SIÈCLE", /*5*/
+	"SONARS", /*6*/
+	"ECLIPTIC", /*7*/
+	"STROBOCOP", /*8*/
+	"COUNTACH", /*9*/
+	"DANCE AUTOMATON DANCE", /*10*/
+	"HORTON HEARS A WHO", /*11*/
+	"CALL ME CHEESUS", /*12*/
+	"MUTE IN E", /*13*/
+	"CHICO Y CONSUELA", /*14*/
+	"WORLD DOMINO CHAMPIONSHIP", /*15*/
+	"DANCE AUTOMATON DANCE", /*16*/
+	"SEARCHING", /*17*/
+	"IRIS", /*18*/
+	"OUTLINES", /*19*/
+	"SUPERNAL", /*20*/
+]
